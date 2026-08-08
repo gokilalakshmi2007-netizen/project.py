373879cbcad8706f4d5bcdccfdbdc1ecc6c0cdfa
gokilalakshmi2007-netizen <gokilalakshmi2007@gmail.com>
2026-08-08 12:09:29 +0530

    updated

--- ecommercepro/urls.py
+++ ecommercepro/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from operator import index
+
 from django.contrib import admin
 from django.urls import path
+from customerapp.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("index",index ,name="index"),
 ]
